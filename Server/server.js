f273c14ae3ea825e6fafd25b821b67d30b837606
Collin Rottinghaus <collinrottinghaus@collinmbp.local>
2022-05-12 23:20:03 -0500

    New p5 version, moved npm control to top level

--- Server/server.js
+++ Server/server.js
@@ -6,6 +6,6 @@ var express = require('express');
 var app = express();
 var server = app.listen(portNum);
 
-app.use(express.static('../Public'));
+app.use(express.static('Public'));
 
 console.log("Server running on port: " + portNum + "!\n");
